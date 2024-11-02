426e71caf626f3fa964f9917a0e98a2587b530a5
Reicher <robin.o.reicher@gmail.com>
2024-11-02 23:14:59 +0100

    LevelManager, 2 levels, win/loose
    Add baseLevel to be daddy of all levels, Add LevelManager to keep track of them and figure out when you have won/lost

--- script/level.gd
+++ script/level.gd
@@ -5,11 +5,9 @@ signal win
 
 func _on_goal_body_entered(body: Node2D) -> void:
 	if body.name == "SoyBoy":
-		print("BOY WIN!")
 		emit_signal("win")
 
 
 func _on_game_over_body_entered(body: Node2D) -> void:
 	if body.name == "SoyBoy":
-		print("Boy LOOSE!")
 		emit_signal("lost")
---
+++ script/levelManager.gd
@@ -0,0 +1,58 @@
+extends Node2D
+
+var levels = []
+var current_level
+var currentLevelNum = 1
+var soyboy_scene = preload("res://scene/soyBoy.tscn")
+var soyboy
+
+func _ready() -> void:
+	load_levels()
+	start_level(currentLevelNum)
+
+func load_levels() -> void:
+	var level_num = 1
+	while true:
+		var level_path = "res://scene/level/level%d.tscn" % level_num
+		if ResourceLoader.exists(level_path):
+			var scene = load(level_path)
+			if scene and scene is PackedScene:
+				levels.append(scene)
+				level_num += 1
+			else:
+				print("Failed to load level at path: ", level_path)
+				break
+		else:
+			break  # No more levels found
+
+func start_level(level_num: int) -> void:
+	# Clean up previous instances
+	if current_level:
+		current_level.queue_free()
+	if soyboy:
+		soyboy.queue_free()
+
+	# Load the new level
+	if level_num > levels.size():
+		print("No more levels!")
+		return
+	current_level = levels[level_num - 1].instantiate()
+	current_level.win.connect(_on_level_win)
+	current_level.lost.connect(_on_level_lost)
+	add_child(current_level)
+
+	# Instantiate SoyBoy
+	soyboy = soyboy_scene.instantiate()
+	soyboy.position = Vector2(0, 0)  # Or use spawn point logic as shown above
+	soyboy.name = "SoyBoy"
+	current_level.add_child(soyboy)
+
+func _on_level_win() -> void:
+	print("Level won!")
+	currentLevelNum += 1
+	start_level(currentLevelNum)
+
+func _on_level_lost() -> void:
+	print("Level lost!")
+	# Restart the current level
+	start_level(currentLevelNum)
