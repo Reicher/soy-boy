a4a88ed5dd4cae1d48b2427562c26d6d0cdd6a93
Reicher <robin.o.reicher@gmail.com>
2024-11-10 22:32:35 +0100

    cleanup simplifications

--- script/level.gd
+++ script/level.gd
@@ -3,11 +3,19 @@ extends Node2D
 signal lost
 signal win
 
+var soyboy_scene = preload("res://scene/soyBoy.tscn")
+var soyboy
+
+func _ready() -> void:
+	soyboy = soyboy_scene.instantiate()
+	soyboy.position = Vector2(0, 0)
+	add_child(soyboy)
+
 func _on_goal_body_entered(body: Node2D) -> void:
-	if body.name == "SoyBoy":
+	if body == soyboy:
 		emit_signal("win")
 
 
 func _on_game_over_body_entered(body: Node2D) -> void:
-	if body.name == "SoyBoy":
+	if body == soyboy:
 		emit_signal("lost")
--- script/levelManager.gd
+++ script/levelManager.gd
@@ -3,8 +3,6 @@ extends Node2D
 var levels = []
 var current_level
 var currentLevelNum = 1
-var soyboy_scene = preload("res://scene/soyBoy.tscn")
-var soyboy
 
 func _ready() -> void:
 	load_levels()
@@ -29,23 +27,16 @@ func start_level(level_num: int) -> void:
 	# Clean up previous instances
 	if current_level:
 		current_level.queue_free()
-	if soyboy:
-		soyboy.queue_free()
 
 	# Load the new level
 	if level_num > levels.size():
 		print("No more levels!")
 		return
+		
 	current_level = levels[level_num - 1].instantiate()
 	current_level.win.connect(_on_level_win)
 	current_level.lost.connect(_on_level_lost)
 	add_child(current_level)
-
-	# Instantiate SoyBoy
-	soyboy = soyboy_scene.instantiate()
-	soyboy.position = Vector2(0, 0)  # Or use spawn point logic as shown above
-	soyboy.name = "SoyBoy"
-	current_level.add_child(soyboy)
 
 func _on_level_win() -> void:
 	print("Level won!")
